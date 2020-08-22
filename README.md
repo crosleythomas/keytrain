97861d6fc608a0c1a43ce892216e0d2f4d3d81e0
Thomas <crosleythomas@gmail.com>
2020-08-22 13:53:39 -0700

    Start of data model

--- README.md
+++ README.md
@@ -41,3 +41,48 @@ git push
 ```
 
 You will see either :white_check_mark: or :x: in response to your answer.
+
+## Design
+
+### Data Model
+
+The primary abstractions are:
+* `Deck` - the group of Cards a user is training with currently. A Deck can have Cards
+from one or more Packs.
+* `Pack` - a group of Cards stored together in a logical group
+* `Card` - the atomic unit of a concept to be trained on, containing one or more Turns
+* `Turn` - a Prompt with an expected series of Responses (answers)
+* `Prompt` - the prompt given to the user to solicit a response
+* `Response` - the expected input from the user to answer the `Prompt` correctly
+
+### Flashcard Data Storage Format
+
+All Cards are represented in a text file representing a `Pack`.  A `Pack` text
+file has the following format:
+
+```
+Name of Card 1
+P: This is a prompt that will be given to a user for a single response prompt
+R: This is the expected Reponse
+
+Name of Card 2
+P: This is a prompt with multiple expected responses
+R: This is the first expected Response
+R: This is the second expected Response
+
+Name of Card 3
+P: This is the first Prompt for a card with multiple Prompts
+R: This is a response
+P: This is the second Prompt
+R: This is the first response to the second Prompt
+R: This is the second response to the second Prompt
+P: This could go on forever
+R: But it won't
+```
+
+
+## Brainstorm
+
+* Filter cards to train on (ex: only cards I missed last time, cards with expectation
+of getting right < 80%, cards I haven't practiced with before, etc.). Implement this both
+for the full deck (global CLI arg) and with per Pack option (CLI option to each Pack)
---
+++ src/common/packs.py
@@ -0,0 +1,73 @@
+import random
+
+class Deck(object):
+    """
+    A Deck is created using cards from one or more packs
+    """
+    def __init__(self, pack_names):
+        self.packs = [Pack(n) for n in pack_names]
+
+    def __init__(self, packs):
+        self.packs = packs
+
+class Pack(object):
+    """
+    Represents a single flash card pack - a grouping of related Cards
+    created by a user.
+    """
+    def __init__(self, name):
+        pass
+
+    def __init__(self, cards):
+        self.cards = cards
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+class Card(object):
+    """
+    Contains one or more Turns
+
+    The atomic unit of a concept to be learned.  A Card can take many forms,
+    but it:
+        * Cannot be broken into smaller pieces when shuffling a Pack
+        * Is always asked in a deterministic order
+        * Does not take any input to its first step
+        * Must implement some form of equality
+
+    Types of cards:
+        * Single question, single answer
+        * Single question, multi-part answer
+        * Multi-part question, response, question, response
+
+    Brainstorm
+        * Make a Card aware of what pack it came from
+        * For multi-step answers, provide abstraction so we can track where
+            in the answer the user gets the question wrong
+        * Make it easy to implement different forms of equality (strict equals, wild-carded, regex, etc)
+        * Ability to store metadata about the card so we can add features in the future
+            * ex - average difficulty, expected difficulty for current user, last guessed currectly
+    """
+    def __init__(self, lines):
+        """
+        Constructor to create a card based on a set of raw data "lines"
+        :param lines:
+        """
+        pass
+
+class Turn(object):
+    """
+    Contains a single Prompt with one or more Responses
+    """
+    def __init__(self, prompt, responses):
+        self.prompt = prompt
+        self.responses = responses
+        pass
+
+class Prompt(object):
+    def __init__(self):
+        pass
+
+class Response(object):
+    def __init__(self):
+        pass
